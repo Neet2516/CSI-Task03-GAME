73127f4e51f0f3202936eb569b91327db1a2f12f
Neet2516 <getneet.25@gmail.com>
2025-10-04 17:54:20 +0530

    Almost done and currently added the local storage with the desclaimer file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import k from "./kaplayCtx.js";
+import k from "./kaplayCtx";
+import disclaimer from "./scenes/disclaimer";
+import game from "./scenes/game";
+import gameover from "./scenes/gameover";
 import mainMenu from "./scenes/mainMenu";
 
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
@@ -11,27 +14,19 @@ k.loadSprite("sonic", "graphics/sonic.png", {
     jump: { from: 8, to: 15, loop: true, speed: 100 },
   },
 });
-k.loadSprite("rings", "graphics/ring.png", {
+k.loadSprite("ring", "graphics/ring.png", {
   sliceX: 16,
   sliceY: 1,
   anims: {
-    run: {
-      spin: {
-        from: 0, to: 15, loop: true, speed: 30
-      },
-    },
-  }
-}) ;
+    spin: { from: 0, to: 15, loop: true, speed: 30 },
+  },
+});
 k.loadSprite("motobug", "graphics/motobug.png", {
   sliceX: 5,
   sliceY: 1,
   anims: {
-    run: {
-      spin: {
-        from: 0, to: 4, loop: true, speed: 8
-      },
-    },
-  }
+    run: { from: 0, to: 4, loop: true, speed: 8 },
+  },
 });
 k.loadFont("mania", "fonts/mania.ttf");
 k.loadSound("destroy", "sounds/Destroy.wav");
@@ -41,8 +36,9 @@ k.loadSound("jump", "sounds/Jump.wav");
 k.loadSound("ring", "sounds/Ring.wav");
 k.loadSound("city", "sounds/city.mp3");
 
+k.scene("disclaimer", disclaimer);
+k.scene("main-menu", mainMenu);
+k.scene("game", game);
+k.scene("gameover", gameover);
 
-k.scene("main-menu",mainMenu);
-k.scene("game",()=>{});
-k.scene("gameover",()=>{});
-k.go("main-menu");
+k.go("disclaimer");
